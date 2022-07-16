6e4f218668cdb8af7e00546a8f756e97fe16db1e
norhan <nora.moh.2011@gmail.com>
2022-07-16 03:58:51 +0200

    feat: add first E2E Cypress for hello world

--- v_3/tests/e2e/specs/HelloWorld.cy.js
+++ v_3/tests/e2e/specs/HelloWorld.cy.js
@@ -1,8 +1,8 @@
 // https://docs.cypress.io/api/table-of-contents
 
-describe("My First Test", () => {
-  it("Visits the app root url", () => {
-    cy.visit("/");
+describe("HelloWorld Test", () => {
+  it("Visits the HelloWorld app root url", () => {
+    cy.visit("/home");
     cy.contains("h1", "Welcome to Your Vue.js App");
   });
 });
